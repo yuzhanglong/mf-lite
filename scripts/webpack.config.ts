4db516b2f55e28ab882c74c34bf8d9bb129dd234
yuzhanglong <yuzl1123@163.com>
2021-08-01 01:52:33 +0800

    fix: fix wrong dynamic import

--- scripts/webpack.config.ts
+++ scripts/webpack.config.ts
@@ -1,59 +1,34 @@
 import * as webpack from 'webpack';
-import HtmlWebpackPlugin = require('html-webpack-plugin')
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as path from 'path';
 
 const config: webpack.Configuration = {
   mode: 'development',
-  entry: './hello/index.ts',
+  entry: './src/index.ts',
   output: {
     filename: '[name].[contenthash:8].js',
-    chunkFilename: '[name].[contenthash:8].chunk.js'
+    chunkFilename: '[name].[contenthash:8].chunk.js',
   },
   devtool: 'source-map',
   plugins: [
-    new webpack.ProvidePlugin({
-      'intl': [
-        path.resolve(
-          process.cwd(),
-          'src/packages/intl/intl-entry'
-        ),
-        'default'
-      ]
-    }),
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
   ],
-  optimization: {
-    splitChunks: {
-      // 选择哪些块进行优化, 提供 all 味着即使在异步和非异步块之间也可以共享块
-      chunks: 'all',
-
-      // 自定义拆分块的名称，webpack 默认配置即可
-      name: false,
-
-      cacheGroups: {
-        // 默认配置下的入口 vendor 名字又臭又长，这里对此作出修改，通过 hash 值来保证不会冲突
-        defaultVendors: {
-          filename: (pathData) => {
-            return `vendor-${pathData.chunk.hash}.js`;
-          }
-        }
-      }
-    }
-  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      '@': path.resolve(process.cwd(), 'src')
-    }
+      '@': path.resolve(process.cwd(), 'src'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader'
-      }
-    ]
-  }
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+    ],
+  },
 };
 
 export default config;
