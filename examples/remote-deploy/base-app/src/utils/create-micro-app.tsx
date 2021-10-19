feec38a7c87a9ffd078027fa0666eea430e8ee5c
yuzhanglong <yuzl1123@163.com>
2021-10-19 22:31:35 +0800

    feat: add loading area for micro app

--- examples/remote-deploy/base-app/src/utils/create-micro-app.tsx
+++ examples/remote-deploy/base-app/src/utils/create-micro-app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { MicroApp, MicroAppRef } from '@mf-lite/core/esm/browser/micro-app';
 import { MICRO_APPS, MicroAppConfig } from '~src/common/const';
 
@@ -16,24 +16,30 @@ export const createMicroApp = (name: string) => {
   return () => {
     const ref = useRef<MicroAppRef>(null);
 
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
     const microAppConfig = {
       name: name,
       entry: getMicroApp(name).url,
-    }
+    };
 
     useEffect(() => {
       if (ref.current?.appStore) {
         ref.current?.appStore.microApp?.loadPromise
           .then(() => {
             console.log('micro app loaded!');
+            setIsLoading(false);
           });
       }
     }, []);
 
     return (
-      <MicroApp
-        ref={ref}
-        microAppConfig={microAppConfig} />
+      <div>
+        {isLoading && 'loading...'}
+        <MicroApp
+          ref={ref}
+          microAppConfig={microAppConfig} />
+      </div>
     );
   };
 };
